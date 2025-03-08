64cc0e31ef30523949f4a370884cc845b811bf11
muhammed2022 <105856253+muha202201@users.noreply.github.com>
2025-03-08 19:44:52 +0300

    Add files via upload

---
+++ footerTest.java
@@ -0,0 +1,28 @@
+package mainTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class footerTest{
+    private WebDriver driver;
+
+    @BeforeClass
+
+    public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+}
